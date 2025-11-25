1be3713563e7cd771344b65fa0ea40be74099f4f
gptshainguse-afk <gptshainguse@gmail.com>
2025-11-25 20:56:41 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css'; 
 import { 
   Plane, Hotel, MapPin, Users, Calendar, 
   Utensils, AlertTriangle, Map, DollarSign, 
